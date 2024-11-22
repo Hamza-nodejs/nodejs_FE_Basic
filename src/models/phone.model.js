2f9c856a1168c39900bbeb779955efb181adafb0
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-11-22 11:36:52 +0500

    fixes

--- src/models/phone.model.js
+++ src/models/phone.model.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const moment = require('moment-timezone');
+const mongoose = require('mongoose')
+const moment = require('moment-timezone')
 
 const phoneSchema = new mongoose.Schema(
     {
@@ -28,22 +28,22 @@ const phoneSchema = new mongoose.Schema(
     {
         timestamps: true,
     }
-);
+)
 
 // Middleware to set EST timestamps
 phoneSchema.pre('save', function (next) {
-    const now = moment.tz('America/New_York').format();
-    this.createdAtEST = this.createdAtEST || now;
-    this.updatedAtEST = now;
-    next();
-});
+    const now = moment.tz('America/New_York').format()
+    this.createdAtEST = this.createdAtEST || now
+    this.updatedAtEST = now
+    next()
+})
 
 phoneSchema.pre('findOneAndUpdate', function (next) {
-    const now = moment.tz('America/New_York').format();
-    this._update.updatedAtEST = now;
-    next();
-});
+    const now = moment.tz('America/New_York').format()
+    this._update.updatedAtEST = now
+    next()
+})
 
-const Phone = mongoose.model('Phone', phoneSchema);
+const Phone = mongoose.model('Phone', phoneSchema)
 
-module.exports = Phone;
+module.exports = Phone
