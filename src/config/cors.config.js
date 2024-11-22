d6b6f50c41c08fe56eda557445b1a7ad229bfcee
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-11-22 14:31:30 +0500

    config.cors()

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,8 +1,11 @@
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
+    exposedHeaders: ['Authorization', 'Content-Length', 'X-Foo'],
     credentials: true,
-}
+    maxAge: 86400,
+    optionsSuccessStatus: 204,
+};
 
-module.exports = corsOptions
+module.exports = corsOptions;
