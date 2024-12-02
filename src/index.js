94d0c1a45e78839e8bf6efacd02ab28e92aa0dbb
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-12-02 14:20:41 +0500

    fixes

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(helmet())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 sessionConfig(app)
-app.use(routes)
+app.use('/api', routes)
 
 
 app.use((req, res) => {
