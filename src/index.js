59936a432c83177674c37288ac9f1d3a56928011
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-12-02 14:17:26 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -5,28 +5,28 @@ const cors = require('cors')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
 const StatusCodes = require('http-status-codes')
+const morgan = require('morgan')
 
 const corsOptions = require('./config/cors.config')
 const sessionConfig = require('./middleware/session')
 const connectToDatabase = require('./config/mongoose.connection')
-const { logger, logRequestDuration } = require('./utils/logger')
 const routes = require('./routes/index')
 
 connectToDatabase()
 const app = express()
 let PORT = process.env.PORT
 
-app.use((req, res, next) => {
-    logger.info(`${req.method} ${req.url}`)
-    next()
-})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - [:date[iso]]'))
+
 
 app.use(cors(corsOptions))
 app.use(helmet())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 sessionConfig(app)
-app.use('/api', routes)
+app.use(routes)
+
+
 app.use((req, res) => {
     return res.status(404).json({
         statusCode: StatusCodes.NOT_FOUND,
